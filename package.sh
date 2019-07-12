de98a9afa7751800d08c3aecb3d85eba90a03d85
jmorris-pillar <jmorris2@pillartechnology.com>
2019-07-12 11:29:03 -0400

    Refactor to properly use chart version (#25)
    co-authored-by: Sukaant Chaudhary <sukaant.chaudhary@gmail.com>

--- package.sh
+++ package.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 chart="${1:?CHART NAME REQUIRED}"
-version="${2:?VERSION REQUIRED}"
 
 helm dependency update $chart
-helm package $chart --version $version --destination docs
+helm package $chart --destination docs
 helm repo index docs
