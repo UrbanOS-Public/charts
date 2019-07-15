a7086cd774e814232bf8fe3cc412b397d3043561
Jessie Morris <jmorris2@pillartechnology.com>
2019-07-15 10:11:38 -0400

    Revert "Refactor to properly use chart version (#25)"
    This reverts commit de98a9afa7751800d08c3aecb3d85eba90a03d85.

--- package.sh
+++ package.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 chart="${1:?CHART NAME REQUIRED}"
+version="${2:?VERSION REQUIRED}"
 
 helm dependency update $chart
-helm package $chart --destination docs
+helm package $chart --version $version --destination docs
 helm repo index docs
