5984de6b1524678ca89c9d0b790135bafc918b55
Scott Millard <smillard@hntb.com>
2019-07-16 16:55:00 -0400

    Update package.sh to merge index instead of update entire index

--- package.sh
+++ package.sh
@@ -2,6 +2,11 @@
 
 chart="${1:?CHART NAME REQUIRED}"
 
-helm dependency update $chart
-helm package $chart --destination docs
-helm repo index docs
+mkdir tmp_charts
+helm package $chart -u -d tmp_charts
+helm repo index --merge docs/index.yaml tmp_charts
+
+mv tmp_charts/*.tgz docs
+mv tmp_charts/index.yaml docs/index.yaml
+
+rm -rf tmp_charts
