1988da500e71a50172078997fe6b79ad5f117940
Ryan Conaway <ryan.r.conaway@accenture.com>
2022-09-20 19:03:23 -0400

    Working cron job

--- charts/updater-cron/test.sh
+++ charts/updater-cron/test.sh
@@ -5,6 +5,7 @@ REPOSITORY=smartcitiesdata/$APP_NAME
 TOKEN=$(curl -s "https://auth.docker.io/token?service=registry.docker.io&scope=repository:$REPOSITORY:pull" | jq -r .token)
 echo "TOKEN:"
 echo $TOKEN
+
 REMOTE_DIGEST=$(curl -s -D - -H "Authorization: Bearer $TOKEN" -H "Accept: application/vnd.docker.distribution.manifest.v2+json" https://index.docker.io/v2/$REPOSITORY/manifests/development | grep docker-content-digest | cut -d ' ' -f 2 | tr -d '[:space:]')
 echo "REMOTE_DIGEST:"
 echo $REMOTE_DIGEST
@@ -15,6 +16,8 @@ echo "LOCAL_APP_PODS:"
 echo $LOCAL_APP_PODS
 
 for pod in ${LOCAL_APP_PODS[@]}; do
+  echo "POD:"
+  echo "$pod"
   LOCAL_DIGEST=$(kubectl get pod --namespace=dev $pod -o json | jq '.status.containerStatuses[] | .imageID ' | cut -d '@' -f 2 | sed 's/"//g' | tr -d '[:space:]')
   echo "LOCAL_DIGEST:"
   echo $LOCAL_DIGEST
