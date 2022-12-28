bbb94a7dfc8adb1a92d9c59753aa8d5234c90295
Chris Duncan <c.m.duncan@accenture.com>
2022-12-28 09:44:58 -0600

    Finish adding e2e postman collection

--- charts/sauron/newman/e2e_test.sh
+++ charts/sauron/newman/e2e_test.sh
@@ -2,28 +2,10 @@
 
 echo "STARTING E2E TESTS"
 
-# safety cleanup in case previous run failed
-newman run e2e_test_cleanup.postman_collection.json --insecure --delay-request 2000
-
 # setup
-newman run e2e_test_setup.postman_collection.json --insecure --delay-request 2000 \
-    --reporters json --reporter-json-export results/setup.json
-sleep 10
+newman run e2e_test.postman_collection.json --insecure --delay-request 2000 \
+    --reporters json --reporter-json-export results.json
 
-# get results
-newman run e2e_test_discovery_api.postman_collection.json --insecure \
-    --reporters json --reporter-json-export results/discovery.json
+FAILURES=$(jq .run.stats.assertions.failed results.json)
 
-# cleanup
-newman run e2e_test_cleanup.postman_collection.json --insecure --delay-request 2000 \
-    --reporters json --reporter-json-export results/cleanup.json
-
-SETUP_FAILURES=$(jq .run.stats.assertions.failed results/setup.json)
-DISCOVERY_FAILURES=$(jq .run.stats.assertions.failed results/discovery.json)
-CLEANUP_FAILURES=$(jq .run.stats.assertions.failed results/cleanup.json)
-
-echo "SETUP_FAILURES:" $SETUP_FAILURES
-echo "DISCOVERY_FAILURES:" $DISCOVERY_FAILURES
-echo "CLEANUP_FAILURES:" $CLEANUP_FAILURES
-
-echo "E2E TEST COMPLETE"
+echo "E2E TEST FAILURES:" $FAILURES
