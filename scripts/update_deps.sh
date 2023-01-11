7755272598f50e5d73ae6ef8ae1c0867f11506f8
Ben Mitchinson <mitchinson.dev@gmail.com>
2023-01-11 11:24:47 -0700

    helm update convenience script

---
+++ scripts/update_deps.sh
@@ -0,0 +1,13 @@
+# to be run from the root of this repo.
+# sometimes dependent charts don't respond to running
+# `helm dependency update` from the `/urban-os` chart.
+# This series of commands perform the updates bottom up.
+
+# when a new chart is added to this repo of charts, and 
+# that new chart has external dependencies listed in it's
+# chart.yaml, it should be added here.
+
+helm dep up kafka
+helm dep up persistence
+helm dep up monitoring
+helm dep up urban-os
